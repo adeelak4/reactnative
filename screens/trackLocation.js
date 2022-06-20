49b0c254404532d05daecc374c881d24e16e23ca
Saqlain1020 <saqlainprinters@gmail.com>
2022-06-20 10:51:45 +0500

    Added current location

--- screens/trackLocation.js
+++ screens/trackLocation.js
@@ -2,10 +2,13 @@ import { StyleSheet, Text, View, Dimensions } from "react-native";
 import MapView, { Geojson, Circle, Marker } from "react-native-maps";
 import React from "react";
 import useBus from "../hooks/useBus";
+import Dot from "../assets/dot.png";
+import * as Location from "expo-location";
 
 const trackLocation = ({ navigator }) => {
   const { location, selectedName } = useBus();
   const [coordinates, setCoordinates] = React.useState([67.1154, 24.9455]);
+  const [myLocation, setMyLocation] = React.useState([1, 1]);
 
   const myPlace = {
     type: "FeatureCollection",
@@ -21,9 +24,22 @@ const trackLocation = ({ navigator }) => {
     ],
   };
 
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setMyLocation([location.coords.longitude, location.coords.latitude]);
+  };
+
   React.useEffect(() => {
     setCoordinates([Number(location?.longitude) || 67.1154, Number(location?.latitude) || 24.9455]);
   }, [location]);
+
+  React.useEffect(() => {
+    setInterval(() => getLocation(), 1000);
+  }, []);
 
   return (
     <View>
@@ -37,6 +53,12 @@ const trackLocation = ({ navigator }) => {
         }}
       >
         <Geojson geojson={myPlace} strokeColor="red" fillColor="green" strokeWidth={2} title={selectedName} />
+        <Marker
+          coordinate={{ latitude: myLocation[1], longitude: myLocation[0] }}
+          title={"title"}
+          description={"Des"}
+          image={Dot}
+        />
       </MapView>
     </View>
   );
--- util/constants.js
+++ util/constants.js
@@ -1,8 +1,8 @@
 const REALTIME_SERVER = "https://gps-sender-backend.herokuapp.com";
 const REALTIME_SERVER_DEVELOPMENT = "http://localhost:8000";
 
-export const SERVER_URL = process.env.NODE_ENV === "production" ? REALTIME_SERVER : REALTIME_SERVER_DEVELOPMENT;
-// export const SERVER_URL = REALTIME_SERVER;
+// export const SERVER_URL = process.env.NODE_ENV === "production" ? REALTIME_SERVER : REALTIME_SERVER_DEVELOPMENT;
+export const SERVER_URL = REALTIME_SERVER;
 
 export const SocketEvent = {
   CONNECT: "connect",
